816d613b784bc12da00ce21e11874c14e6ad72ef
Asiama Phoebe <phoebeasiama6@gmail.com>
2024-12-07 16:42:18 +0000

    correction

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+// src/constants/uiText.ts
+export const BUTTON_TEXTS = {
+      BOOK_NOW: 'Book Now',
+      DETAILS: 'View Details',
+      SUBMIT: 'Submit',
+    };
+    
+    export const ERROR_MESSAGES = {
+      NETWORK_ERROR: 'Failed to connect. Please try again later.',
+      VALIDATION_ERROR: 'Please fill out all required fields correctly.',
+    };
+    
+    export const PAGE_TITLES = {
+      HOME: 'Welcome to ALX Listing App',
+      PROPERTY_DETAILS: 'Property Details',
+    };
+    
